7c977330943a47d23f89b6bb0eee224230e3abfa
Eric Clemmons <eric@nova.(none)>
2009-09-16 04:02:39 +0000

    Fixed fmylife bookmarklets

--- recipes/fmylife.js
+++ recipes/fmylife.js
@@ -1,13 +1,15 @@
-jQuery('.pagination:last a:last').depagify({
-    trigger: '#footer',
-    container: '#wrapper',
-    effect: function() {
-        jQuery(this).fadeIn('slow');
-    },
-    request: function(options) {
-        jQuery('.pagination', options.container).remove();
-    },
-    success: function(event, options) {
-        jQuery('#ad_leaderboard', options.container).remove();
-    }
-});
+;(function($) {
+    $('.pagination:last a:last').depagify({
+        trigger: '#footer',
+        container: '#wrapper',
+        effect: function() {
+            jQuery(this).fadeIn('slow');
+        },
+        request: function(options) {
+            jQuery('.pagination', options.container).remove();
+        },
+        success: function(event, options) {
+            jQuery('#ad_leaderboard', options.container).remove();
+        }
+    });
+})(jQuery);
